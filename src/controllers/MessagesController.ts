8a1e7fdf0ffad7ae1bbb3b7f082b39210d43e49c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-21 13:54:01 -0300

    Feat: list messages by user id

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -15,6 +15,16 @@ class MessagesController {
 
     return response.json(message);
   }
+
+  async showAllByUser(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const messagesService = new MessagesService();
+
+    const list = await messagesService.listByUser(id);
+
+    return response.json(list);
+  }
 }
 
 export default MessagesController;
