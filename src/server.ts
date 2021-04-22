0309880791d1c62a0036988c09e51332b7366bc2
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-22 13:52:02 -0300

    Feat: add web socket initial config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 import './database';
 import routes from './routes';
 
 const server = express();
 
+const http = createServer(server); // Criando protocolo http
+const io = new Server(http); // Criando protocolo websocket
+
+io.on('connection', (socket: Socket) => {
+  console.log('Connected', socket.id);
+});
+
 server.use(express.json());
 
 server.use(routes);
 
-server.listen(3030, () => console.log('Server is running'));
+http.listen(3030, () => console.log('Server is running'));
