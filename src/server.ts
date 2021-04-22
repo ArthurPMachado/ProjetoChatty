04972fc3fe05bd4641543f4aa4819ea33c442d18
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-22 14:00:07 -0300

    Feat: config to use html in node

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
+import path from 'path';
 
 import './database';
 import routes from './routes';
 
 const server = express();
+
+server.use(express.static(path.join(__dirname, '..', 'public')));
+server.set('views', path.join(__dirname, '..', 'public'));
+server.engine('html', require('ejs').renderFile);
+
+server.set('view engine', 'html');
 
 const http = createServer(server); // Criando protocolo http
 const io = new Server(http); // Criando protocolo websocket
