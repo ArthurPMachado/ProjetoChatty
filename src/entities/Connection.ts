568f17a499e78c2bd4a1c933c8e04629a90e9df9
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-22 18:01:33 -0300

    Feat: add connection entity

---
+++ src/entities/Connection.ts
@@ -0,0 +1,38 @@
+import {
+  Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+import User from './User';
+
+@Entity()
+class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  socket_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export default Connection;
